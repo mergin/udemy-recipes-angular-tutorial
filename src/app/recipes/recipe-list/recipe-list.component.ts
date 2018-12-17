34cc95f2e7a7bb638f88749b72082e933abf9144
David Enrique Prieto Melendez <dprietme@everis.com>
2018-12-17 09:33:45 +0100

    Added basic recipe layout

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
     selector: 'app-recipe-list',
     templateUrl: './recipe-list.component.html',
     styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+    recipes: Recipe[] = [
+        new Recipe(
+            'A test Recipe',
+            'This is simply a test',
+            // tslint:disable-next-line:max-line-length
+            'https://http2.mlstatic.com/corte-para-hallacas-produccion-nacional-60-unidades-para-60-D_NQ_NP_869908-MLA25589543992_052017-F.webp'
+        ),
+        new Recipe(
+            'A test Recipe',
+            'This is simply a test',
+            // tslint:disable-next-line:max-line-length
+            'https://http2.mlstatic.com/corte-para-hallacas-produccion-nacional-60-unidades-para-60-D_NQ_NP_869908-MLA25589543992_052017-F.webp'
+        ),
+        new Recipe(
+            'A test Recipe',
+            'This is simply a test',
+            // tslint:disable-next-line:max-line-length
+            'https://http2.mlstatic.com/corte-para-hallacas-produccion-nacional-60-unidades-para-60-D_NQ_NP_869908-MLA25589543992_052017-F.webp'
+        )
+    ];
 
     constructor() { }
 
