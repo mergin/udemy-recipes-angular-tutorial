463b93a3f5bcb880ae41a89835f6f2a2332ed6a8
David Enrique Prieto Melendez <dprietme@everis.com>
2019-01-24 16:38:21 +0100

    Changed shopping list form to a template form

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Ingredient } from '@app/models/ingredient.model';
 import { ShoppingListService } from '@app/services/shopping-list.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
     selector: 'app-shopping-edit',
@@ -10,28 +11,26 @@ import { ShoppingListService } from '@app/services/shopping-list.service';
 })
 export class ShoppingEditComponent implements OnInit {
 
-    @ViewChild('nameInput') nameInputRef: ElementRef;
-    @ViewChild('amountInput') amountInputRef: ElementRef;
-
     constructor(private shoppingListService: ShoppingListService) { }
 
     ngOnInit() {
     }
 
-    addIngredient() {
-        if (this.nameInputRef.nativeElement.value && this.amountInputRef.nativeElement.value) {
-            this.shoppingListService.addIngredient(
-                new Ingredient(
-                    this.nameInputRef.nativeElement.value,
-                    Number(this.amountInputRef.nativeElement.value)
+    // add item handler
+    onAddItem(form: NgForm) {
+
+        const value = form.value;
+
+        this.shoppingListService.addIngredient(
+            new Ingredient(
+                value.name,
+                value.amount
             ));
-            this.clearForm();
-        }
+        // this.clearForm();
     }
 
-    private clearForm() {
-        this.nameInputRef.nativeElement.value = '';
-        this.amountInputRef.nativeElement.value = '';
-    }
-
+    // private clearForm() {
+    //     this.nameInputRef.nativeElement.value = '';
+    //     this.amountInputRef.nativeElement.value = '';
+    // }
 }
