e90d0107eb25d28fffa41c3393f804e49db2fde1
David <merginvalhala@gmail.com>
2019-02-04 21:54:20 +0100

    Created recipes module. TODO: dropdown directive fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,42 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { BetterHighlightDirective } from './directives/better-highlight.directive';
 import { UnlessDirective } from './directives/unless.directive';
 import { DropdownDirective } from './directives/dropdown.directive';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { ShortenPipe } from './pipes/shorten.pipe';
 import { FilterPipe } from './pipes/filter.pipe';
 import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';
+import { RecipesModule } from './recipes/recipes.module';
 
 @NgModule({
     declarations: [
         AppComponent,
         HeaderComponent,
-        RecipesComponent,
-        RecipeListComponent,
-        RecipeDetailComponent,
-        RecipeItemComponent,
         ShoppingListComponent,
         ShoppingEditComponent,
         BetterHighlightDirective,
         UnlessDirective,
         DropdownDirective,
-        RecipeStartComponent,
-        RecipeEditComponent,
         ShortenPipe,
         FilterPipe,
         SignupComponent,
@@ -45,9 +34,9 @@ import { SigninComponent } from './auth/signin/signin.component';
     imports: [
         BrowserModule,
         HttpClientModule,
-        AppRoutingModule,
         FormsModule,
-        ReactiveFormsModule
+        RecipesModule,
+        AppRoutingModule
     ],
     providers: [],
     bootstrap: [AppComponent]
