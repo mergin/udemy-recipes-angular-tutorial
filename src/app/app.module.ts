63acb61926abfbf67cf5de1559db45643b87b246
David <merginvalhala@gmail.com>
2019-02-07 22:59:39 +0100

    Added shopping and auth modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { BetterHighlightDirective } from './directives/better-highlight.directive';
 import { UnlessDirective } from './directives/unless.directive';
 import { ShortenPipe } from './pipes/shorten.pipe';
 import { FilterPipe } from './pipes/filter.pipe';
-import { SignupComponent } from './auth/signup/signup.component';
-import { SigninComponent } from './auth/signin/signin.component';
 import { RecipesModule } from './recipes/recipes.module';
+import { ShoppingListModule } from './shopping-list/shopping-list.module';
+import { AuthModule } from './auth/auth.module';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
     declarations: [
         AppComponent,
         HeaderComponent,
-        ShoppingListComponent,
-        ShoppingEditComponent,
         BetterHighlightDirective,
         UnlessDirective,
         ShortenPipe,
-        FilterPipe,
-        SignupComponent,
-        SigninComponent
+        FilterPipe
     ],
     imports: [
         BrowserModule,
         HttpClientModule,
-        FormsModule,
         RecipesModule,
+        ShoppingListModule,
+        AuthModule,
         AppRoutingModule,
         SharedModule
     ],
