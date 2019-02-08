b0c9c42e3f6154b53dc97afed5b3cb0cfcf0561c
David <merginvalhala@gmail.com>
2019-02-08 17:44:29 +0100

    Added core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { BetterHighlightDirective } from './directives/better-highlight.directive';
 import { UnlessDirective } from './directives/unless.directive';
 import { ShortenPipe } from './pipes/shorten.pipe';
 import { FilterPipe } from './pipes/filter.pipe';
-import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { AuthModule } from './auth/auth.module';
 import { SharedModule } from './shared/shared.module';
-import { HomeComponent } from './home/home.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
     declarations: [
         AppComponent,
-        HeaderComponent,
         BetterHighlightDirective,
         UnlessDirective,
         ShortenPipe,
-        FilterPipe,
-        HomeComponent
+        FilterPipe
     ],
     imports: [
         BrowserModule,
@@ -31,7 +27,8 @@ import { HomeComponent } from './home/home.component';
         ShoppingListModule,
         AuthModule,
         AppRoutingModule,
-        SharedModule
+        SharedModule,
+        CoreModule
     ],
     providers: [],
     bootstrap: [AppComponent]
